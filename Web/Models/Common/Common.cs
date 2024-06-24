894aec13feb45ccfca7f9b8d0225418250f9979f
George <georgeey123@gmail.com>
2024-06-24 21:28:37 +0000

    feat(backend): add models

---
+++ Web/Models/Common/Common.cs
@@ -0,0 +1,8 @@
+namespace Web.Models.Common;
+
+public class Common
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ Web/Models/Tasks.cs
@@ -0,0 +1,8 @@
+namespace Web.Models;
+
+public class Tasks
+{
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime? Due { get; set; }
+}
